a5a878ede0fb97b19748efe0d5e758522604722a
Kerjox <60511422+Kerjox@users.noreply.github.com>
2020-09-21 12:50:38 +0200

    Día 1 v1

---
+++ src/Ejercicio01.java
@@ -0,0 +1,12 @@
+public class Ejercicio01 {
+
+    public static void main(String arg[]) {
+        int x;
+        int y;
+        int res;
+        x = 55; // '=' es el operador de asignación
+        y = 78;
+        res = x + y;
+        System.out.println("El resultado es: " + res);
+    }
+}

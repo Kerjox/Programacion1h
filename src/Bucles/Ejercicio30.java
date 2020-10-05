2cf465fa9a9118b1dd8b505f6971396e11039691
Kerjox <kerjoxgamer095@gmail.com>
2020-10-05 13:07:01 +0200

    05-oct-20

--- src/Bucles/Ejercicio30.java
+++ src/Bucles/Ejercicio30.java
@@ -10,12 +10,12 @@ public class Ejercicio30 {
 
     public static void main(String[] args) {
 
-        int datos[][] = {{45, 21, 3, 10, 99}, {2, 41, 20, 10, 1}, {10, 56, 12, 78, 4}};
+        float datos[][] = {{45.2f, 21.6f, 3.4f, 10.8f, 99.43f}, {2.6f, 41.1f, 20.2f, 10.7f, 1.33f}, {10, 56, 12, 78, 4}};
         String productos[] = {"Naranjas", "Peras", "Manzanas"};
 
         for (int i = 0; i < productos.length; i++) {
 
-            int suma = 0;
+            float suma = 0;
 
             for (int o = 0; o < datos[i].length; o++) {
 
@@ -23,7 +23,7 @@ public class Ejercicio30 {
 
             }
 
-            System.out.printf("Se han vendiodo %d kilos de %s \n", suma, productos[i]);
+            System.out.printf("Se han vendiodo %.2f kilos de %s \n", suma, productos[i]);
 
         }
 

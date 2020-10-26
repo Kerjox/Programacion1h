f2c422d9c281909e92070eba4f7e19147a52298d
Kerjox <kerjoxgamer095@gmail.com>
2020-10-26 13:04:17 +0100

    26-oct-20

---
+++ src/Funciones/Ejercicio56.java
@@ -0,0 +1,60 @@
+/*
+    Lo mismo que en el 55 solo que hay que meter los valores dentro de una nueva tabla llamada ventas "sales"
+ */
+
+package Funciones;
+
+import java.util.Arrays;
+
+public class Ejercicio56 {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Calcular el dinero que se ha ganado con cada producto todos lo días con la tabla de precios \n");
+
+        int[][] kgProductos = {
+                {20, 15, 13, 16, 2 , 4, 6},
+                {4, 1, 5, 7, 4, 5, 7}
+        };
+        int[] pricesKg = {2, 4};
+        int[][] sales = doProfitsPerDay(kgProductos, pricesKg);
+
+        System.out.printf("\n Dataos iniciales \n");
+
+        printArray(kgProductos);
+
+        System.out.printf("\n Dataos multiplicados \n");
+
+        printArray(sales);
+
+    }
+
+    private static int[][] doProfitsPerDay(int[][] data, int[] price) {
+
+        int[][] sales = new int[data.length][data[0].length];
+
+        for (int x = 0; x < data.length; x++) {
+
+            for (int y = 0; y < data[0].length; y++) {
+
+                sales[x][y] = data[x][y] * price[x];
+
+            }
+
+        }
+
+        return sales;
+
+    }
+
+    private static void printArray(int[][] data){
+
+        for (int[] array : data){
+
+            System.out.printf("%s \n", Arrays.toString(array));
+
+        }
+
+    }
+
+}

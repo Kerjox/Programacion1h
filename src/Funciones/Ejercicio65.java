abc9dc311ff35d60cf001751b88f055b4faf2b9c
Kerjox <kerjoxgamer095@gmail.com>
2020-10-28 18:21:11 +0100

    28-ocr-20

--- src/Funciones/Ejercicio65.java
+++ src/Funciones/Ejercicio65.java
@@ -6,6 +6,47 @@ package Funciones;
 
 public class Ejercicio65 {
 
+    public static void main(String[] args) {
 
+        System.out.println("Contar cuantas veces se repite una cadena de caracteres dentro de otra cadena de caracteres \n");
+
+        char[] data = new char[] {'n', 'a', 'a', 'x', 'n', 'n', 'n'};
+        char[] thingToFind = new char[] {'a', 'x'};
+
+        System.out.printf("Se repite: '%d' veces", findConcordances(data, thingToFind));
+
+    }
+
+    private static int findConcordances(char[] data, char[] thingToFind){
+
+        int numberOfConcordances = 0;
+
+        for (int x = 0; x < data.length - thingToFind.length + 1; x++) {
+
+            int verifyString = 0;
+
+            for (int i = x, o = 0; o < thingToFind.length; i++, o++) {
+
+                if (data[i] != thingToFind[o]){
+
+                    break;
+
+                }
+
+                verifyString++;
+
+            }
+
+            if (verifyString == thingToFind.length){
+
+                numberOfConcordances++;
+
+            }
+
+        }
+
+        return numberOfConcordances;
+
+    }
 
 }

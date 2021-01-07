49d34e86a0893889ad042b5b6cd3815d0f95fbfe
Kerjox <kerjoxgamer095@gmail.com>
2021-01-07 18:29:25 +0100

    07-ene-21

---
+++ Segunda/src/Ejercicio06/Ventana2.java
@@ -0,0 +1,35 @@
+package Ejercicio06;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ventana2 {
+
+	private JPanel panel1;
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JMenuItem Exit;
+
+	public Ventana2() {
+
+		Exit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame("Ventana2");
+		frame.setContentPane(new Ventana2().panel1);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}

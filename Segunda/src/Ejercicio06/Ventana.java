8887cea22b7cacd427871123f26e0891ebb1a473
Kerjox <kerjoxgamer095@gmail.com>
2021-01-13 13:17:39 +0100

    13-ene-21

--- Segunda/src/Ejercicio06/Ventana.java
+++ Segunda/src/Ejercicio06/Ventana.java
@@ -1,14 +1,20 @@
 package Ejercicio06;
 
+import org.jetbrains.annotations.NotNull;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class Ventana {
 
 	private JPanel panel1;
-	private JMenuItem openMenuItem;
+	private JMenuItem newMenuItem;
+	private JTextArea text;
 	private JMenuItem exitMenuItem;
+	private JMenuItem saveMenuItem;
+	private JMenuItem openMenuItem;
 
 	public Ventana() {
 
@@ -16,14 +22,86 @@ public class Ventana {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+
 				System.exit(0);
+			}
+		});
+
+		newMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				text.setText("");
+			}
+		});
+		openMenuItem.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				JFileChooser openFileWindows = newFileWindow();
+				File file = openFileWindows.getSelectedFile();
+				try {
+					text.setText(getContentFile(file));
+				} catch (IOException ioException) {
+					ioException.printStackTrace();
+				}
 			}
 		});
 	}
 
+	@NotNull
+	private String getContentFile(File file) throws IOException {
+
+		FileReader f = new FileReader(file);
+		BufferedReader b = new BufferedReader(f);
+		String line;
+		String content = "";
+
+		while((line = b.readLine()) != null) {
+
+			content = content.concat(line).concat("\n");
+		}
+
+		b.close();
+		return content;
+	}
+
+	private JFileChooser newFileWindow() {
+
+		JFileChooser openFileWindows = new JFileChooser();
+		openFileWindows.setAcceptAllFileFilterUsed(true);
+		javax.swing.filechooser.FileFilter filter = new javax.swing.filechooser.FileFilter() {
+
+			@Override
+			public boolean accept(File f) {
+
+				if (f.isDirectory()) {
+
+					return true;
+				}else {
+
+					return f.getName().endsWith(".txt");
+				}
+
+			};
+
+			@Override
+			public String getDescription() {
+
+				return "Text file (*.txt)";
+			}
+		};
+
+		openFileWindows.setFileFilter(filter);
+		openFileWindows.showOpenDialog(null);
+		return openFileWindows;
+	}
+
 	public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
 
-		JFrame frame = new JFrame("Ventana");
+		JFrame frame = new JFrame("Window");
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		frame.setContentPane(new Ventana().panel1);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
